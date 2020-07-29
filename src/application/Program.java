e28267121f328ab88477bf0ded344c5f4a2fe8c5
arlon antonio <arlon20092009@gmail.com>
2020-07-28 23:59:39 -0300

    exception trainig

---
+++ src/application/Program.java
@@ -0,0 +1,42 @@
+package application;
+
+import java.util.Scanner;
+
+import model.entities.Account;
+import model.exceptions.DomainException;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		try {
+			System.out.println("Enter account data");
+			System.out.print("Number: ");
+			int number = sc.nextInt();
+			System.out.print("Holder: ");
+			String holder = sc.next();
+			System.out.print("Initial balance: ");
+			sc.nextLine();
+			double balance = sc.nextDouble();
+			System.out.print("Withdraw limit: ");
+			double withdrawLimit = sc.nextDouble();
+
+			Account account = new Account(number, holder, balance, withdrawLimit);
+			System.out.println();
+			System.out.print("Enter amount for withdraw: ");
+			sc.nextLine();
+			double amount = sc.nextDouble();
+			account.withdraw(amount);
+
+			System.out.println(account.toString());
+		} catch (DomainException e) {
+			System.out.println("Withdraw error: " + e.getMessage());
+		} catch (RuntimeException e) {
+			System.out.println("Unexpected error");
+		}
+
+		sc.close();
+
+	}
+
+}
